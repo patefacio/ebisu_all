b7ba87b6f2c73148f7284f7b28b4401b3622b97d
Daniel Davidson <dbdavidson@yahoo.com>
2016-04-27 08:18:41 -0500

    initial commit binding multiple repos into one testable repo

---
+++ bin/add_submodules.sh
@@ -0,0 +1,10 @@
+cd $HOME/dev/open_source/ebisu_jugo
+git submodule add https://github.com/patefacio/id
+git submodule add https://github.com/patefacio/ebisu
+git submodule add https://github.com/patefacio/ebisu_cpp
+git submodule add https://github.com/patefacio/ebisu_cpp_db
+git submodule add https://github.com/patefacio/ebisu_py
+git submodule add https://github.com/patefacio/ebisu_pod
+git submodule add https://github.com/patefacio/ebisu_ang
+git submodule add https://github.com/patefacio/ebisu_capnp
+
